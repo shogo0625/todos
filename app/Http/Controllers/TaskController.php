3e99a607a4c4d1ad70e5c2bbc0107c1867e7ffcf
shogo0625 <[shogo19940625@gmail.com]>
2020-08-22 09:34:54 +0000

    add authentication setting

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -14,58 +14,53 @@ use Illuminate\Support\Facades\Auth;
 class TaskController extends Controller
 {
     //
-    public function index(int $id)
+    public function index(Folder $folder)
     {
         // 対象モデルのallクラスメソッドで取得
         $folders = Auth::user()->folders()->get();
-        // 選ばれたフォルダを取得
-        $current_folder = Folder::find($id);
         // 選べれたフォルダに紐付いたタスクを取得
-        $tasks = $current_folder->tasks()->get();
+        $tasks = $folder->tasks()->get();
         
         // view関数 第1引数：渡すファイル名　第2引数：配列（キーが参照する際の変数名）
         return view('tasks/index', [
             'folders' => $folders,
-            'current_folder_id' => $current_folder->id,
+            'current_folder_id' => $folder->id,
             'tasks' => $tasks,
         ]);
     }
     
-    public function showCreateForm(int $id)
+    public function showCreateForm(Folder $folder)
     {
         return view('tasks/create', [
-            'folder_id' => $id,
+            'folder_id' => $folder->id,
         ]);
     }
     
-    public function create(int $id, CreateTask $request)
+    public function create(Folder $folder, CreateTask $request)
     {
-        $current_folder = Folder::find($id);
-        
         $task = new Task();
         $task->title = $request->title;
         $task->due_date = $request->due_date;
         
-        $current_folder->tasks()->save($task);
+        $folder->tasks()->save($task);
         
         return redirect()->route('tasks.index', [
-            'id' => $current_folder->id,
+            'folder' => $folder->id,
         ]);
     }
     
-    public function showEditForm(int $id, int $task_id)
+    public function showEditForm(Folder $folder, Task $task)
     {
-        $task = Task::find($task_id);
+        $this->checkRelation($folder, $task);
         
         return view('tasks/edit', [
             'task' => $task,
         ]);
     }
     
-    public function edit(int $id, int $task_id, EditTask $request)
+    public function edit(Folder $folder, Task $task, EditTask $request)
     {
-        // リクエストされたIDでタスクデータを取得
-        $task = Task::find($task_id);
+        $this->checkRelation($folder, $task);
         
         // 編集対象のタスクに入力値をsave
         $task->title = $request->title;
@@ -75,8 +70,15 @@ class TaskController extends Controller
         
         // 編集したタスクが属するタスク一覧画面へリダイレクト
         return redirect()->route('tasks.index', [
-            'id' => $task->folder_id,
+            'folder' => $task->folder_id,
         ]);
+    }
+    
+    public function checkRelation(Folder $folder, Task $task)
+    {
+        if ($folder->id !== $task->folder_id) {
+            abort(404);
+        }
     }
     
 }
--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,19 @@ Route::group(['middleware' => 'auth'], function() {
     // Route::get('URL', 'controller@method')->name('このURLを参照できるようにする名前');
     Route::get('/', 'HomeController@index')->name('home');
     
-    Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
-    
     Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
     Route::post('/folders/create', 'FolderController@create');
-    
-    Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
-    Route::post('/folders/{id}/tasks/create', 'TaskController@create');
-    
-    Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
-    Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+
+    // ポリシーをミドルウェアを介して使用する
+    Route::group(['middleware' => 'can:view,folder'], function() {
+        Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
+
+        Route::get('/folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+        Route::post('/folders/{folder}/tasks/create', 'TaskController@create');
+        
+        Route::get('/folders/{folder}/tasks/{task}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+        Route::post('/folders/{folder}/tasks/{task}/edit', 'TaskController@edit');
+    });
 });
 
 // 会員登録・ログイン・ログアウト・パスワード再設定のルーティング定義
